fd81068eadf62657e6f44e23818d10abc8ee510b
ErickStoppa <erick.s.s.martins31@gmail.com>
2025-05-17 16:03:02 -0300

    feat(banco de dados): criação das models, repositories e string de conexão com banco de dados

---
+++ AgendaApp/Data/Database.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace AgendaApp.Data
+{
+    public static class Database
+    {
+        private const string ConnString =
+            "Server=ERICK-PC;Database=AgendaDB;User Id=sa;Password=Teste123;";
+        public static IDbConnection GetConnection()
+            => new SqlConnection(ConnString);
+    }
+}
---
+++ AgendaApp/Models/Compromisso.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AgendaApp.Models
+{
+    public class Compromisso
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataHora { get; set; }
+    }
+}

57b7382e897f406a7fd8ea60746958ead0036eb1
ErickStoppa <erick.s.s.martins31@gmail.com>
2025-05-17 16:04:45 -0300

    feat(services): criação do serviço de autenticação com segurança

---
+++ AgendaApp/Services/AuthService.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+
+namespace AgendaApp.Services
+{
+    public static class AuthService
+    {
+        public static void CriarSenha(string senha, out byte[] hash, out byte[] salt)
+        {
+            salt = new byte[16];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            using (var pbkdf2 = new Rfc2898DeriveBytes(senha, salt, 10000))
+            {
+                hash = pbkdf2.GetBytes(32);
+            }
+        }
+
+        public static bool VerificarSenha(string senha, byte[] hash, byte[] salt)
+        {
+            using (var pbkdf2 = new Rfc2898DeriveBytes(senha, salt, 10000))
+            {
+                return pbkdf2.GetBytes(32).SequenceEqual(hash);
+            }
+        }
+    }
+}
